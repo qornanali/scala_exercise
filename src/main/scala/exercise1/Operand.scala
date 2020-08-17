85c6e328de2872fef697e7f727156fbb1883a0e9
Ali Qornan <aliqornan@ymail.com>
2020-08-17 22:12:45 +0700

    [qornanali][exercise1] Set the second argument to be called by name

--- src/main/scala/exercise1/Operand.scala
+++ src/main/scala/exercise1/Operand.scala
@@ -1,11 +1,11 @@
 package exercise1
 
 object Operand {
-  def and(x: Boolean, y: Boolean): Boolean = {
+  def and(x: Boolean, y: => Boolean): Boolean = {
     if (x) y else false
   }
 
-  def or(x: Boolean, y: Boolean): Boolean = {
+  def or(x: Boolean, y: => Boolean): Boolean = {
     if (x) x else y
   }
 }
